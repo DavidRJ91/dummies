4ee48662e54e806040f4f9157b3c516aab42789e
winadmin <drj00@iesemilidarder.com>
2018-01-09 17:44:00 +0100

    1.1

--- dummyweb/src/main/java/com/iesemilidarder/fordummies/web/Launcher.java
+++ dummyweb/src/main/java/com/iesemilidarder/fordummies/web/Launcher.java
@@ -1,6 +1,10 @@
 package com.iesemilidarder.fordummies.web;
+import com.iesemilidarder.fordummies.core.DBObject;
 import com.iesemilidarder.fordummies.core.Users;
 import org.eclipse.jetty.server.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Request;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +13,8 @@ import static spark.Spark.*;
 
 public class Launcher   {
     private static List<Users> IUser = new ArrayList<Users>();
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
+
 
     private static void init(){
         for (int i=0;i<10;i++){
@@ -18,8 +24,15 @@ public class Launcher   {
             user.save();
             IUser.add(user);
         }
+        DBObject dbo = IUser.get(0);
+        log.info("Loading finished");
     }
 
+    private static boolean shouldReturnHtml(Request request) {
+        String accept = request.headers("Accept");
+        return accept != null && accept.contains("text/html");
+    }
+    
     public static void main (String... args){
         init();
         port(8080);
